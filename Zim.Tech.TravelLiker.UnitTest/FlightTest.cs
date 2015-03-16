1b02fe36f267a4c7ab2d4416b0b34b066bd4ab50
Ku Yuen <kuyuen@gmail.com>
2015-03-17 04:14:25 +0800

    1st update

---
+++ Zim.Tech.TravelLiker.UnitTest/FlightTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
+using System.Globalization;
+using System.Runtime.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Zim.Tech.TravelLiker.Common;
+using Zim.Tech.TravelLiker.Travelport.uAPI;
+using uAPIUnit = Zim.Tech.TravelLiker.Travelport.uAPI.Util;
+using uAPIFlight = Zim.Tech.TravelLiker.Travelport.uAPI.Air;
+using Zim.Tech.TravelLiker.Travelport.uAPI.Air;
+
+namespace Zim.Tech.TravelLiker.UnitTest
+{
+    [TestClass]
+    public class FlightTest
+    {
+        public T PrepareHeader<T>() where T : new()
+        {
+            T xRequest = new T();
+            string PropertieName = "AuthorizedBy";
+            if (xRequest.GetType().GetProperties().Any(p => p.Name == PropertieName))
+                xRequest.GetType().GetProperty(PropertieName).SetValue(xRequest, "user", null); //Universal API/uAPI6768275016-96b0fd84
+            PropertieName = "TraceId";
+            if (xRequest.GetType().GetProperties().Any(p => p.Name == PropertieName))
+                xRequest.GetType().GetProperty(PropertieName).SetValue(xRequest, "trace", null);
+            PropertieName = "TargetBranch";
+            if (xRequest.GetType().GetProperties().Any(p => p.Name == PropertieName))
+                xRequest.GetType().GetProperty(PropertieName).SetValue(xRequest, "P7009887", null);
+
+            return xRequest;
+        }
+
+        [TestMethod]
+        public void ReferenceDataRetrieveReq()
+        {
+            //string test_dir = TestContext.TestDir;
+            //string solution_dir = Path.GetDirectoryName(Path.GetDirectoryName(test_dir));
+            string sDllPath = Directory.GetCurrentDirectory();
+
+            uAPIUnit.ReferenceDataRetrieveReq xRequest = new uAPIUnit.ReferenceDataRetrieveReq();
+            xRequest.AuthorizedBy = "Universal API/uAPI6768275016-96b0fd84";
+            xRequest.TraceId = "trace";
+            xRequest.TargetBranch = "P7009887";
+            xRequest.BillingPointOfSaleInfo = new uAPIUnit.BillingPointOfSaleInfo() { OriginApplication = "UAPI" };
+            xRequest.TypeCode = "City";
+        
+            string xml = Serialize<uAPIUnit.ReferenceDataRetrieveReq>.SerializeXmlToString("util", "http://www.travelport.com/schema/util_v25_0", xRequest);
+            //string xml = Serialize<uAPIUnit.ReferenceDataRetrieveReq>.SerializeXmlToString(xRequest);
+            string xmlFile = Path.Combine(sDllPath, "ReferenceDataRetrieveReq.xml");
+            File.WriteAllText(xmlFile, xml);
+        }
+
+
+        [TestMethod]
+        public void LowFareSearchReq()
+        {
+            //string test_dir = TestContext.TestDir;
+            //string solution_dir = Path.GetDirectoryName(Path.GetDirectoryName(test_dir));
+            string sDllPath = Directory.GetCurrentDirectory();
+
+            uAPIFlight.LowFareSearchReq xRequest = PrepareHeader<uAPIFlight.LowFareSearchReq>();
+            xRequest.SolutionResult = true;
+            //uAPIFlight.LowFareSearchReq xRequest = new uAPIFlight.LowFareSearchReq();
+            xRequest.BillingPointOfSaleInfo = new uAPIFlight.BillingPointOfSaleInfo() { OriginApplication = "UAPI" };
+
+            uAPIFlight.SearchAirLeg from = new uAPIFlight.SearchAirLeg();
+            from.SearchOrigin = new typeSearchLocation[1] { (new typeSearchLocation() { Item = new Airport() { Code = "HKG" } }) };
+            from.SearchDestination = new typeSearchLocation[1] { (new typeSearchLocation() { Item = new Airport() { Code = "TPE" } }) };
+            from.Items = new typeFlexibleTimeSpec[1] { (new typeFlexibleTimeSpec() { PreferredTime = "2015-03-24" }) };
+
+            uAPIFlight.SearchAirLeg to = new uAPIFlight.SearchAirLeg();
+            to.SearchOrigin = new typeSearchLocation[1] { (new typeSearchLocation() { Item = new Airport() { Code = "TPE" } }) };
+            to.SearchDestination = new typeSearchLocation[1] { (new typeSearchLocation() { Item = new Airport() { Code = "HKG" } }) };
+            to.Items = new typeFlexibleTimeSpec[1] { (new typeFlexibleTimeSpec() { PreferredTime = "2015-03-29" }) };
+
+            xRequest.Items = new SearchAirLeg[2] { from, to };
+            xRequest.AirSearchModifiers = new AirSearchModifiers() { MaxSolutions = "5" };
+            xRequest.SearchPassenger = new SearchPassenger[1] { new SearchPassenger() { BookingTravelerRef = "gr8AVWGCR064r57Jt0+8bA==", Code = "ADT" } };
+            xRequest.AirPricingModifiers = new AirPricingModifiers() { CurrencyType = "HKD" };
+
+            //string xml = Serialize<uAPIFlight.LowFareSearchReq>.SerializeXmlToString(xRequest);
+            List<XmlNamespace> xmlNamespaces = new List<XmlNamespace>() { 
+                (new XmlNamespace() { NameSpace = "air", Url="http://www.travelport.com/schema/air_v25_0"}), 
+                (new XmlNamespace() { NameSpace = "com", Url="http://www.travelport.com/schema/common_v25_0"})
+            };
+            string xml = Serialize<uAPIFlight.LowFareSearchReq>.SerializeXmlToString(xmlNamespaces, xRequest);
+            string xmlFile = Path.Combine(sDllPath, "LowFareSearchReq.xml");
+            File.WriteAllText(xmlFile, xml);
+        }
+
+        [TestMethod]
+        public void LowFareSearchResp()
+        {
+            string CurrencyType = string.Empty;
+            string sDllPath = Directory.GetCurrentDirectory();
+            string xmlFile = Path.Combine(sDllPath, "LowFareSearchRsp.xml");
+            string xmlcontents = System.IO.File.ReadAllText(xmlFile);
+            uAPIFlight.LowFareSearchRsp xResp = Serialize<uAPIFlight.LowFareSearchRsp>.DeserializeXmlFromStringWithoutNamespace(xmlcontents);
+            //XmlDocument doc = new XmlDocument();
+            //doc.PreserveWhitespace = true;
+            //doc.Load(xmlFile);
+            //xmlcontents = doc.InnerXml;
+
+            //uAPIFlight.LowFareSearchRsp xResp = Serialize<uAPIFlight.LowFareSearchRsp>.DeSerializeXmlFromFile(xmlFile);
+            if (xResp != null)
+                CurrencyType = xResp.CurrencyType;
+        }
+    }
+}
