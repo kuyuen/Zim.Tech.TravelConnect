e83a3740f64ba7ec9d2f7a0c35bc88f4b7c56d69
Ku Yuen <kuyuen@gmail.com>
2015-04-04 18:51:42 +0800

    Create Booking Response Class

---
+++ Zim.Tech.TravelConnect/Booking/AirReservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Reflection;
+
+namespace Zim.Tech.TravelConnect.Booking
+{
+    #region AirReservation Class
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.0.30319.34230")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class AirReservation
+    {
+    }
+    #endregion
+}
---
+++ Zim.Tech.TravelConnect/Booking/BookingTraveler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Reflection;
+
+namespace Zim.Tech.TravelConnect.Booking
+{
+    #region BookingTraveler Class
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Xml", "4.0.30319.34230")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    public partial class BookingTraveler
+    {
+    }
+    #endregion
+}
+
